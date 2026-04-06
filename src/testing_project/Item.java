28994d17fba3b31304aa32f68e2dfe2e85358824
Blake <bwilfong@depaul.edu>
2026-04-06 11:49:24 -0500

    feat: add equals() and hashCode() to Item because Item is hashMap key

--- src/testing_project/Item.java
+++ src/testing_project/Item.java
@@ -1,5 +1,7 @@
 package testing_project;
 
+import java.util.Objects;
+
 public class Item {
 	private final String name;
 	private final int price; // in cents
@@ -14,4 +16,16 @@ public class Item {
 	
 	public int getPrice() {return this.price;}
 	
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Item)) return false;
+        Item other = (Item) obj;
+        return price == other.price && Objects.equals(name, other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
 }
