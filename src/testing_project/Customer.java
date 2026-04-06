002cf19b50d11f4b6bfd59811339314704f88b55
Blake <bwilfong@depaul.edu>
2026-04-06 11:12:15 -0500

    feat: Customer

---
+++ src/testing_project/Customer.java
@@ -0,0 +1,13 @@
+package testing_project;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private String state;
+	
+	public Customer(String firstName, String lastName, String state) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.state = state;
+	}
+}
