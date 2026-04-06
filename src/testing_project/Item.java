2277c1766cc1d9d799dc25389e55a037b6cccf99
Blake <bwilfong@depaul.edu>
2026-04-06 11:45:51 -0500

    fix: removed setters from Item to make them immutable. changed quantity check in ShoppingCart to <= 0.

--- src/testing_project/Item.java
+++ src/testing_project/Item.java
@@ -1,8 +1,8 @@
 package testing_project;
 
 public class Item {
-	private String name;
-	private int price; // in cents
+	private final String name;
+	private final int price; // in cents
 	
 	
 	public Item(String name, int price) {
@@ -12,14 +12,6 @@ public class Item {
 	
 	public String getName() {return this.name;}
 	
-	public void setName(String newName) {
-		this.name = newName;
-	}
-	
 	public int getPrice() {return this.price;}
-	
-	public void setPrice(int newPrice) {
-		this.price = newPrice;
-	}
 	
 }
