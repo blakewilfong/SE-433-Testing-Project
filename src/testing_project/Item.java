7384ad7cb24fd0063405b9fe9c310a8381e1194d
Blake <bwilfong@depaul.edu>
2026-04-06 11:37:51 -0500

    feat: add getters and setters to price, begin implementation of ShoppingCart

--- src/testing_project/Item.java
+++ src/testing_project/Item.java
@@ -9,4 +9,17 @@ public class Item {
 		this.name = name;
 		this.price = price;
 	}
+	
+	public String getName() {return this.name;}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	
+	public int getPrice() {return this.price;}
+	
+	public void setPrice(int newPrice) {
+		this.price = newPrice;
+	}
+	
 }
